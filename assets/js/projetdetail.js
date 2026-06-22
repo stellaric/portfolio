149df861dd93b809b3a3edcccea80fd8a162a72c
stellaric <stella.richal.sio@gmail.com>
2026-06-22 18:02:08 +0200

    modification page

--- assets/js/projetdetail.js
+++ assets/js/projetdetail.js
@@ -1,42 +1,17 @@
 const projects = {
 
     1: {
-        title: "Projet E-commerce",
+        title: "Projet E-commerce Weboxing ",
 
         subtitle: "Site moderne et responsive",
 
-        image: "https://picsum.photos/id/1/1200/700",
+        image: "../media/projet/weboxing/logo_weboxing.png",
 
         description:
         "Ce projet consistait à créer une boutique en ligne moderne avec une expérience utilisateur fluide et immersive.",
 
-        tags: ["HTML", "CSS", "JavaScript", "UI Design"]
-    },
-
-    2: {
-        title: "Application Fitness",
-
-        subtitle: "Application mobile intuitive",
-
-        image: "https://picsum.photos/id/2/1200/700",
-
-        description:
-        "Une application permettant de suivre les entraînements et les performances sportives.",
-
-        tags: ["Mobile", "UX", "Design", "Prototype"]
-    },
-
-    3: {
-        title: "Portfolio Créatif",
-
-        subtitle: "Portfolio premium moderne",
-
-        image: "https://picsum.photos/id/3/1200/700",
-
-        description:
-        "Création d’un portfolio immersif avec animations modernes et identité visuelle forte.",
-
-        tags: ["Portfolio", "Branding", "CSS", "Animation"]
+        tags: ["HTML", "CSS", "JavaScript", "UI Design"] ,
+        link: "https://github.com/stellaric/weboxing"
     }
 
 };
