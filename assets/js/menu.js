ca131a593eeacdb40070468adc8bb29d784811dd
stellaric <stella.richal.sio@gmail.com>
2026-08-16 23:32:02 +0200

    modifier page a propos et blog

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -32,3 +32,30 @@ document.querySelectorAll(".nav-links a")
     });
 
 });
+
+/* =========================
+   LIEN ACTIF
+========================= */
+
+const currentPage =
+    window.location.pathname.split("/").pop();
+
+const navLinks =
+    document.querySelectorAll(
+        ".nav-links a, .nav-links-right a"
+    );
+
+navLinks.forEach(link => {
+
+    const linkPage =
+        link.getAttribute("href")
+            .split("/")
+            .pop();
+
+    if (linkPage === currentPage) {
+
+        link.classList.add("active");
+
+    }
+
+});
